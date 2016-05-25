cebc1cb850b98f798fcd5a9dccd48b29cb7fb6de
James Leddy <jwleddy@gmail.com>
2016-05-25 10:12:13 -0700

    using a before_filter in the reviews_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :movies do
-    resources :review, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
