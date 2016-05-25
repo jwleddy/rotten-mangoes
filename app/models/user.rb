2cfa764a77ecaac7297630f9866cbb541c5d21d3
James Leddy <jwleddy@gmail.com>
2016-05-24 22:13:46 -0700

    generating review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :reviews
   validates :email, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :password, length: { in: 6..20 }, on: :create
-  
+
 end
