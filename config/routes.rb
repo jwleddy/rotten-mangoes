b9ec4e7b53e0af73078c738664e99b54571baff5
James Leddy <jwleddy@gmail.com>
2016-05-24 22:50:34 -0700

    generating reviews controller and reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  resources :movies
+  resources :movies do
+    resources :review, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
