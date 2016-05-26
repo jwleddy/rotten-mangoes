2c4abed4b4bc4ee93e24939343df97695863b4ba
jwleddy <jwleddy@gmail.com>
2016-05-26 00:04:25 +0000

    finished enhancement 1 (mostly)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  paginates_per 10
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :email, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
