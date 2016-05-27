5f11fbf72fc74fa9539600f4a0f3f74385d9aa66
jwleddy <jwleddy@gmail.com>
2016-05-27 01:28:59 +0000

    minor changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
 
   has_many :reviews
+
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
@@ -8,7 +9,9 @@ class Movie < ActiveRecord::Base
   validates :poster_image_url, presence: true
   validates :release_date, presence: true
   validate :release_date_is_in_the_past
+
   mount_uploader :poster_image, PosterImageUploader
+
   scope :search_title_director, ->(term) { where('upper(title) LIKE ? OR upper(director) LIKE ?', term, term) }
   scope :under_90, -> { where('runtime_in_minutes <= 90') }
   scope :between_90_120, -> { where('runtime_in_minutes > 90 and runtime_in_minutes <= 120') }
@@ -16,7 +19,7 @@ class Movie < ActiveRecord::Base
 
   def review_average
     if reviews.size > 0
-      reviews.sum(:rating_out_of_ten)/reviews.size
+      (reviews.sum(:rating_out_of_ten)/reviews.size.to_f).round(1)
     else
       0
     end
--- app/models/review.rb
+++ app/models/review.rb
@@ -8,5 +8,6 @@ class Review < ActiveRecord::Base
   validates :rating_out_of_ten, numericality: { only_integer: true }
   validates :rating_out_of_ten, numericality: { greater_than_or_equal_to: 1 }
   validates :rating_out_of_ten, numericality: { less_than_or_equal_to: 10 }
+  validates :user_id, uniqueness: { scope: [:movie_id] }
 
 end
